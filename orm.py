89032a1b81f919bdb52cf530fb6c507e3f2dd957
= <iglezgallego@gmail.com>
2024-01-29 21:27:14 +0100

    3

--- orm.py
+++ orm.py
@@ -1,24 +1,71 @@
 # Importar la biblioteca tkinter y usar el alias tk
 import tkinter as tk
+import random
+import math
+import json
+import sqlite3
+
+personas = []
+numeropersonas = 20
 
 class Persona:
     # Crear metodo constructor
     def __init__(self):
         #Propiedades de un objeto que voy a querer guardar en una bbdd
-        self.posx = 512
-        self.posy = 512
+        #Cada vez que se ejecute el programa la posición será aletoria
+        self.posx = random.randint(0,1024)
+        self.posy = random.randint(0,1024)
+        self.radio = 30
+        self.direccion = random.randint(0,360)
+        self.color = "blue"
+        self.entidad = ""
+        
     def dibuja(self):
-        lienzo.create_oval(30,30,60,60,fill="red")
+        self.entidad = lienzo.create_oval(
+            self.posx-self.radio/2,
+            self.posy-self.radio/2,
+            self.posx+self.radio/2,
+            self.posy+self.radio/2,
+            fill=self.color)
+        
+    def mueve(self):
+        self.colisiona()
+        lienzo.move(
+            self.entidad,
+            math.cos(self.direccion),
+            math.sin(self.direccion))
+        #Actualizo las posiciones
+        self.posx += math.cos(self.direccion)
+        self.posy += math.sin(self.direccion)
+        
+    def colisiona(self):
+        if self.posx < 0 or self.posx > 1024 or self.posy < 0 or self.posy > 1024:
+            self.direccion += math.pi
 
-# Crear una instancia de la ventana principal
+# Creo la ventana principal
 raiz = tk.Tk()
-
+#En la ventana creo un lienzo
 lienzo = tk.Canvas(raiz,width=1024,height=1024)
 lienzo.pack()
 
-#Creo una instancia de la clase
-persona = Persona()
-persona.dibuja()
+#En la coleccion introduzco instancias de personas
+for i in range(0,numeropersonas):
+    personas.append(Persona())
+    
+#Dibuja cada una de las instancias de personas
+for persona in personas:
+    persona.dibuja()
+    
+#Creo un bucle repetitivo    
+def bucle():
+    #Mover las personas en el bucle
+    for persona in personas:
+        persona.mueve()
+    #En 1s quiero ejecutar de nuevo el bucle
+    raiz.after(10,bucle)
+    
+#Ejecuto el bucle    
+bucle()
 
 raiz.mainloop()
 
